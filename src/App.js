d3eac8220f8b0a061f0aa2a4f4e6f9251effd113
kalppatel12 <kalppatelwork.10@gmail.com>
2023-09-22 16:57:47 +0530

    laptop-box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
-
+import LowerDiv from "./components/LowerDiv";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <LowerDiv></LowerDiv>
+    </div>
+  );
 };
 
 export default App;
