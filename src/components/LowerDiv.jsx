638a3ff8448d4fce81a18592b76ff2e9f68c3256
mohdshahzil <theshahzil@gmail.com>
2023-09-22 19:50:40 +0530

    few-corrections

--- src/components/LowerDiv.jsx
+++ src/components/LowerDiv.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const LowerDiv = () => {
   return (
-    <div className='flex-row flex w-full bg-black'>
-      <div className='bg-slate-50 w-2/4'>
-       <div className=' w-2/4  text-center text-white bg-slate-500 '>
-        Text Area
-       </div>
-      </div>
-      <div className=' w-2/4 bg-transparent'>
-        <img className="" src="/images/laptopscreen.jpg" alt="laptop-screen"></img>
+    <div className="flex flex-row justify-between items-center">
+      <div className="w-3/6 flex items-center justify-center "></div>
+      <div className="w-3/6 flex items-center justify-center min-h-fit  ">
+        {" "}
+        <img
+          src="/images/myLaptop.png"
+          className="object-contain"
+          alt=""
+        />{" "}
       </div>
     </div>
   );
-}
+};
 
 export default LowerDiv;
-
