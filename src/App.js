fde6ab208908bbedc28787a6daa18668d5d4deca
mohdshahzil <theshahzil@gmail.com>
2023-09-22 15:23:22 +0530

    navbar-added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import Navbar from "./components/navbar";
 const App = () => {
-  return <div className="bg-custom-1 h-screen w-screen ">App</div>;
+  return (
+    <div className="bg-custom-1 h-screen w-screen ">
+      <Navbar></Navbar>
+    </div>
+  );
 };
 
 export default App;
-
-
