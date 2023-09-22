4e3e6939e10065e5890abaffc490ca1d51fc6b2c
mohdshahzil <theshahzil@gmail.com>
2023-09-22 15:12:05 +0530

    background-color-added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return <div className="bg-custom-1 h-screen w-screen ">App</div>;
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        "custom-1": "#FFF0F5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
