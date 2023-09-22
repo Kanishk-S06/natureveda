5bee1402fbffd1f9a2124933f90cf62272ba563b
Kanishk <Kanishksharma2609@gmail.com>
2023-09-22 19:21:17 +0530

    Contact-us addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
+import ContactUs from "./components/ContactUs";
+
 const App = () => {
+  const [con, setCon] = useState(true);
+
+  const Contactus = (e) => {
+    e.preventDefault();
+    setCon(false);
+  };
+
+  const onsubmit = (e) => {
+    e.preventDefault();
+    setCon(true);
+  };
+
   return (
-    <div className="bg-custom-1 h-screen w-screen ">
-      <Navbar></Navbar>
-      <Hero></Hero>
+    <div className="bg-custom-1 h-screen w-screen">
+      {con ? (
+        <>
+          <Navbar Contactus={Contactus} />
+          <Hero />
+        </>
+      ) : (
+        <ContactUs onsubmit={onsubmit} />
+      )}
     </div>
   );
 };
