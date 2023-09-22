3d1f694020e27b3e7758e862d6582028bd1c345a
Kanishk <Kanishksharma2609@gmail.com>
2023-09-22 15:14:06 +0530

    navbar addition

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import PhoneFrame from './components/phoneframe';
+import Navbar from './components/navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App h-screen flex justify-center items-center bg-gray-200">
+    //   <PhoneFrame>
+    //     {/* Add the content you want inside the phone frame */}
+    //     <div className="p-4">
+    //       {/* Your app's content goes here */}
+    //       <h1 className="text-white text-2xl">Hello, Phone Frame!</h1>
+    //     </div>
+    //   </PhoneFrame>
+    // </div>
+    <div>
+      <Navbar></Navbar>
     </div>
   );
 }
 
 export default App;
+
+
