f84b7fa6e9514b11b4452e7090f7272e07238fd6
mohdshahzil <theshahzil@gmail.com>
2023-09-22 16:48:35 +0530

    hero-section-completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Hero from "./components/Hero";
 const App = () => {
   return (
     <div className="bg-custom-1 h-screen w-screen ">
       <Navbar></Navbar>
+      <Hero></Hero>
     </div>
   );
 };
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-transparent p-4">
+    <nav className="bg-transparent p-8">
       <div className="container mx-auto flex justify-between items-center ">
-        <a href="/" className="text-2xl font-nunito font-semibold text-black">Dr. NatureVeda</a>
-        
+        <a href="/" className="text-2xl font-nunito font-semibold text-black">
+          Dr. NatureVeda
+        </a>
+
         <ul className="flex space-x-4">
-          <li><a href="/" className="text-white hover:text-gray-300 font-lato"></a>Home</li>
-          <li><a href="/about" className="text-white hover:text-gray-300 font-lato"></a>About</li>
-          <li><a href="/services" className="text-white hover:text-gray-300 font-lato"></a>Contact</li>
-          <li><a href="/contact" className="text-white hover:text-gray-300 font-lato"></a>Help</li>
+          <li>
+            <a
+              href="/"
+              className="text-white hover:text-gray-300 font-lato"
+            ></a>
+            Home
+          </li>
+          <li>
+            <a
+              href="/about"
+              className="text-white hover:text-gray-300 font-lato"
+            ></a>
+            About
+          </li>
+          <li>
+            <a
+              href="/services"
+              className="text-white hover:text-gray-300 font-lato"
+            ></a>
+            Contact us{" "}
+          </li>
         </ul>
       </div>
     </nav>
