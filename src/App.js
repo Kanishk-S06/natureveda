63746f816ae825be55d8daea62cb42c89bfc14f0
Kanishk <Kanishksharma2609@gmail.com>
2023-09-22 15:15:53 +0530

    conflict-resolution

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import PhoneFrame from './components/phoneframe';
 import Navbar from './components/navbar';
 function App() {
   return (
-    // <div className="App h-screen flex justify-center items-center bg-gray-200">
-    //   <PhoneFrame>
-    //     {/* Add the content you want inside the phone frame */}
-    //     <div className="p-4">
-    //       {/* Your app's content goes here */}
-    //       <h1 className="text-white text-2xl">Hello, Phone Frame!</h1>
-    //     </div>
-    //   </PhoneFrame>
-    // </div>
     <div>
-      <Navbar></Navbar>
     </div>
   );
 }
